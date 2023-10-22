890f604f376b722d86d847e0f846fcf897c3353f
phucdo2601 <ngocphucdo2601@gmail.com>
2023-10-22 11:01:33 +0700

    [listFunProb01] 02. First basic create component in react native b01

---
+++ LearnFunReactNaB01/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View, SafeAreaView } from 'react-native'
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>Current weather</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  )
+}
+
+export default App
