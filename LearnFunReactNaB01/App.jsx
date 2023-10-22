77a1006bb822b84187a8ea98db0e7e8a8fe898ba
phucdo2601 <ngocphucdo2601@gmail.com>
2023-10-22 11:08:56 +0700

    [listFunProb01] 03. first learn basic styling in react native b01

--- LearnFunReactNaB01/App.jsx
+++ LearnFunReactNaB01/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
-import { Text, View, SafeAreaView } from 'react-native'
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native'
 
 const App = () => {
   return (
     <>
-      <SafeAreaView>
-        <View>
+      <SafeAreaView style={styles.wrapper}>
+        <View style={styles.container}>
           <Text>Current weather</Text>
         </View>
       </SafeAreaView>
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  wrapper: {
+    backgroundColor: 'yellow',
+    flex: 1,
+    alignItems: 'center'
+  }
+})
 
 export default App
