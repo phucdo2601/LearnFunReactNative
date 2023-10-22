07c38438e7f1553cd05fe8452bf556c9120eee7a
phucdo2601 <ngocphucdo2601@gmail.com>
2023-10-22 14:00:22 +0700

    [listFunProb01] 04. First build basic the weather app screen b01

--- LearnFunReactNaB01/App.jsx
+++ LearnFunReactNaB01/App.jsx
@@ -7,6 +7,17 @@ const App = () => {
       <SafeAreaView style={styles.wrapper}>
         <View style={styles.container}>
           <Text>Current weather</Text>
+          <Text style={styles.temp}>6</Text>
+          <Text style={styles.feels}>Feel like 5</Text>
+
+          <View style={styles.highLowWrapper}>
+            <Text style={styles.highLow}>Hign: 8 </Text>
+            <Text style={styles.highLow}>Low: 6</Text>
+          </View>
+        </View>
+        <View style={styles.bodyWrapper}>
+          <Text style={styles.description}>Its sunny</Text>
+          <Text style={styles.message}>Its perfect t-shirt weather</Text>
         </View>
       </SafeAreaView>
     </>
@@ -14,13 +25,38 @@ const App = () => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
   wrapper: {
-    backgroundColor: 'yellow',
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  container: {
     flex: 1,
     alignItems: 'center'
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    fontSize: 30,
+    color: 'black'
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
   }
 })
 
